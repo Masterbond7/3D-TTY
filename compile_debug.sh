e8f9b2786431738c7a730e9247b9a3dc663948e7
Masterbond7 <jamesslough4@gmail.com>
2023-09-12 11:30:43 +1200

    Added window border and a box that bounces left to right (does not remove previous frame)

---
+++ compile_debug.sh
@@ -0,0 +1,2 @@
+echo "Compiling..."
+g++ ./src/main.cpp -Wall -g -lncursesw -o ./bin/app.o
--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,49 @@
 #include <iostream>
 #include <ncurses.h>
+#include "./ascii_box.hpp"
+#include <unistd.h>
 
 // Main function
 int main() {
     // Initialize ncurses
+    setlocale(LC_ALL, ""); // Needed for extended ASCII
     initscr(); // Init screen
     cbreak(); // Live user input (could use raw())
     noecho(); // Stops user input appearing
     keypad(stdscr, TRUE); // Enable FN & arrow keys etc
+    curs_set(0); // Make the terminal cursor invisible
 
     // Get terminal resolution
     int w_height, w_width;
     getmaxyx(stdscr, w_height, w_width);
 
-    // Print terminal size
-    mvprintw(0, 0, "#"); mvprintw(w_height-1, w_width-1, "#");
-    mvprintw(1, 1, "Resolution is %dx%d", w_width, w_height);
-    refresh();
+    // Make box and make it go
+    int dir = 1;
+    int x = 1;
 
-    // End program on user input
-    getch();
+    // Graphics loop
+    while (true) {
+        // Draw window border
+        draw_box(stdscr,0,0,w_width,w_height);
+
+        // Print terminal size
+        mvprintw(1, 1, "The overall resolution is %dx%d", w_width, w_height);
+
+        // Moving box
+        if (x+9==w_width-1) {dir=-1;}
+        if (x==1) {dir=1;}
+        draw_box(stdscr, x, 5, 9, 3);
+        mvprintw(6,x+1,"Box! c:");
+        x+=dir;
+
+        // Display update
+        refresh();
+
+        // Delay
+        usleep(1000000/30); // 30fps
+    }
+
+    // End program
     endwin();
 
     // Exit Successfully
