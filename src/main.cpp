e3573ff21765a7218658a8d2cd929e8becdf4769
Masterbond7 <jamesslough4@gmail.com>
2023-09-18 09:38:47 +1200

    Removed an extra refresh to make it render faster and added the basis for starting with basic normal based lighting

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <ncurses.h>
-#include "./ascii_box.hpp"
 #include "./moller_trumbore.hpp"
 #include "./vector_math.hpp"
 #include <unistd.h>
@@ -23,16 +22,22 @@ int main() {
     getmaxyx(stdscr, w_height, w_width);
 
     // Triangle
-    double triangle[3][3] = {
+    double triangle[4][3] = { // v0, v1, v2, normal
     {0.0, 0.0, 0.0},
     {0.0, 0.0, 1.0},
-    {0.0, 1.0, 0.0}};
+    {0.0, 1.0, 0.0},
+    {-1.0, 0.0, 0.0}};
 
     // Init variables
     double c_angle[2] = {0.0, 0.0}; // (deg) Angle of rotation from x+, (vert, hor)
     double c_fov = 90.0f; // degrees
     double c_pos[3] = {-5.0, 0.0, 0.0}; //(x,y,z)
-    double c_rdist = 4.0f; // Render distance
+    double c_rdist = 1.0f; // Render distance
+
+    // Init gradient & lighting_delta (angle between camera and face normal
+    double lighting_delta[3] = {0, 0, 0};
+    double lighting_dangle = 0.0;
+    const char gradient[7] = {'-', '^', '!', '%', '>', '$', '#'};
 
     // Graphics loop
     while (true) {
@@ -52,12 +57,6 @@ int main() {
         if (c=='l') {c_angle[1]+=10.0;}
         if (c=='j') {c_angle[1]-=10.0;}
 
-        // Draw window border
-        draw_box(stdscr,0,0,w_width,w_height);
-
-        // Print terminal size
-        mvprintw(1, 1, "The overall resolution is %dx%d", w_width, w_height);
-
         // Go through each column of the screen
         for (int x=0; x<w_width; x++) {
             // Get the camera angle step size
@@ -65,9 +64,7 @@ int main() {
 
             // Get the horizontal angle of the ray
             double h_angle = ((c_fov/-2.0f)+c_angle[1] + (c_angle_step) * x); // Angle is in degrees
-            temp=(c_angle[1]);
             h_angle = (h_angle*M_PI)/180.0; // Converts angle to radians
-
 
             // Initialize ray destination varaibles
             double dest[3];
@@ -89,16 +86,21 @@ int main() {
 
                 // Check for intersection, if = #, else = -
                 char state[2];
-                if (intersects_triangle(c_pos, dest, triangle[0], triangle[1], triangle[2]) == 1) {sprintf(state, "#");}
+                lighting_delta[0] = (triangle[3][0] + dest[0]) / 2; // x
+                lighting_delta[1] = (triangle[3][1] + dest[1]) / 2; // y
+                //lighting_delta[2] = (triangle[3][2] + dest[2]) / 2; // z
+                lighting_dangle = sqrt(pow(lighting_delta[0],2) + pow(lighting_delta[1],2)) / 1.0; // Adj/Hyp; Adj = sqrt(x^2 + y^2); Hyp = 1
+                if (lighting_dangle < 0) {lighting_dangle *= -1;}
+                if (lighting_dangle > 9) {lighting_dangle = ((int)lighting_dangle) % 7;}
+                if (intersects_triangle(c_pos, dest, triangle[0], triangle[1], triangle[2]) == 1) {sprintf(state,"#");}//state[0]=gradient[7-((int)(lighting_dangle*7)-1)]; state[1]='\0';}
                 else {sprintf(state,"-");}
                 mvprintw(y,x,state);
 
                 // Debug
+                temp = lighting_dangle;
                 char debug[256];
-                sprintf(debug, "TMP:%f, Ox:%f, Oy:%f, Oz:%f, Ex:%f, Ey:%f, Ez:%f, Ah:%f, Av:%f",temp, c_pos[0], c_pos[1], c_pos[2], dest[0], dest[1], dest[2], h_angle, v_angle);
+                sprintf(debug, "TMP:%f, Ox:%f, Oy:%f, Oz:%f, Ex:%f, Ey:%f, Ez:%f, Ah:%f, Av:%f",temp, c_pos[0], c_pos[1], c_pos[2], dest[0], dest[1], dest[2], c_angle[1], c_angle[0]);
                 mvprintw(0,0,debug);
-
-                refresh();
 
                 // Delay
                 //usleep(1000000/10000);
